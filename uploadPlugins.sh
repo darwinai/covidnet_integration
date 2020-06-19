40bd2b0399aa502b118af892e193a8de15a791f8
Jeffer Peng <jeffer.peng@darwinai.ca>
2020-06-19 18:53:53 -0400

    added script to upload plugins

---
+++ uploadPlugins.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=local/pl-covidnet \
+ descriptor_file@jsonRepresentations/covidnet.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-covidnet
+
+http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=fnndsc/pl-med2img \
+ descriptor_file@jsonRepresentations/med2img.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-med2img
