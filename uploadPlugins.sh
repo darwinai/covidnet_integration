952dc3b0b69d3304947136d9e9fa1d521c10a67a
janakitti <janakittiratana@gmail.com>
2021-02-24 13:45:03 -0500

    Update uploadPlugins.sh to use appropriate plugin versions, update README

--- uploadPlugins.sh
+++ uploadPlugins.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=local/pl-covidnet \
+http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=fnndsc/pl-covidnet:0.2.0 \
  descriptor_file@jsonRepresentations/covidnet.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-covidnet
 
-http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=fnndsc/pl-med2img \
+http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=fnndsc/pl-med2img:1.1.0.1 \
  descriptor_file@jsonRepresentations/med2img.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-med2img
 
-http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=local/pl-ct-covidnet \
+http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=fnndsc/pl-ct-covidnet:0.2.0 \
  descriptor_file@jsonRepresentations/ct_covidnet.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-ct-covidnet
 
 http -a cubeadmin:cubeadmin1234 -f POST http://localhost:8010/api/v1/plugins/ dock_image=local/pl-pdfgeneration \
  descriptor_file@jsonRepresentations/pdfgeneration.json public_repo=https://github.com/FNNDSC/pl-simplefsapp name=pl-pdfgeneration
-
+ 
