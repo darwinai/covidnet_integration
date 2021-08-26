171262701aaa9d1acedc4fddbc25c9d49f8f4d71
Matthew Wang <mslwang2001@gmail.com>
2021-08-26 15:55:16 -0400

    Update httpie installation

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-pip install httpie
+# Installing httpie through pip is out of date
+# Use the following installation instructions instead
+# https://httpie.io/docs#installation
+# pip install httpie
 pip3 install python-swiftclient
 pip3 install pydicom
 pip3 install python-chrisclient
