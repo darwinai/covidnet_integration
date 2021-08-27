95085c718a123ebe5219b0ade34f7008f39d058d
Matthew Wang <mslwang2001@gmail.com>
2021-08-27 01:41:14 -0400

    Switch os.system for subprocess

--- upload_swift_notify_cube_fnndsc.py
+++ upload_swift_notify_cube_fnndsc.py
@@ -1,15 +1,17 @@
-from os import system, path
+import subprocess
+from os import path
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument('--imageDir', dest='imageDir', help='location of image directory')
 args = parser.parse_args()
 print(path.abspath(args.imageDir))
-system("px-push " + "--swiftIP 127.0.0.1 " + "--swiftPort 8080 " +
-        "--swiftLogin chris:chris1234 " + "--swiftServicesPACS covidnet " +
-        "--swiftPackEachDICOM " + f"--xcrdir {path.abspath(args.imageDir)} " + 
-        "--parseAllFilesWithSubStr dcm " +"--json > push.json")
-system("px-register " + 
-        "--CUBEURL http://localhost:8000/api/v1/ " + "--CUBEusername chris " +
-        "--CUBEuserpasswd chris1234 " + "--swiftServicesPACS covidnet " +
-        "--verbosity 1 " + "--json " + " --logdir /tmp/log " + "--upstreamFile push.json")
+push_json = open("push.json", "w")
+subprocess.run(["px-push", "--swiftIP", "127.0.0.1", "--swiftPort", "8080",
+        "--swiftLogin", "chris:chris1234", "--swiftServicesPACS", "covidnet",
+        "--swiftPackEachDICOM", "--xcrdir", path.abspath(args.imageDir), 
+        "--parseAllFilesWithSubStr", "dcm", "--json"], stdout=push_json)
+subprocess.run(["px-register","--CUBEURL", "http://localhost:8000/api/v1/", "--CUBEusername", "chris",
+ "--CUBEuserpasswd","chris1234", "--swiftServicesPACS", "covidnet",
+ "--verbosity", "1", "--json", "--logdir", "/tmp/log", "--upstreamFile", "push.json"])
+subprocess.run(["rm", "push.json"])
